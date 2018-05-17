1664272d0dbf8fbca858395e32353052e5fb1289
D <s90789@outlook.com>
2018-05-17 23:03:25 +0800

    Add preference

---
+++ common/src/main/java/com/d/lib/common/common/preference/operation/AbstractOp.java
@@ -0,0 +1,25 @@
+package com.d.lib.common.common.preference.operation;
+
+import android.content.SharedPreferences;
+
+/**
+ * Abstract Operation
+ * Created by D on 2018/3/14.
+ */
+public class AbstractOp {
+    protected SharedPreferences settings;
+    protected SharedPreferences.Editor editor;
+
+    public AbstractOp(SharedPreferences settings, SharedPreferences.Editor editor) {
+        this.settings = settings;
+        this.editor = editor;
+    }
+
+    protected void save() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.GINGERBREAD) {
+            editor.apply();
+        } else {
+            editor.commit();
+        }
+    }
+}
