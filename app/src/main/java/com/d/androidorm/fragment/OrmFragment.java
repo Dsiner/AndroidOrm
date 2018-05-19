fa570f806e986ce3d5e6a4c27dd9808f54241ede
D <s90789@outlook.com>
2018-05-19 15:07:58 +0800

    Add title

--- app/src/main/java/com/d/androidorm/fragment/OrmFragment.java
+++ app/src/main/java/com/d/androidorm/fragment/OrmFragment.java
@@ -1,10 +1,13 @@
 package com.d.androidorm.fragment;
 
+import android.view.View;
+
 import com.d.androidorm.R;
 import com.d.androidorm.presenter.OrmPresenter;
 import com.d.androidorm.view.IOrmView;
 import com.d.androidorm.widget.OperateView;
 import com.d.lib.common.module.loader.AbsFragment;
+import com.d.lib.common.utils.ViewHelper;
 import com.d.lib.common.view.TitleLayout;
 
 import butterknife.BindView;
@@ -19,9 +22,34 @@ public abstract class OrmFragment<T> extends AbsFragment<T, OrmPresenter<T>> imp
     @BindView(R.id.ov_operate)
     OperateView ovOperate;
 
+    protected String mTitle;
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        int resId = v.getId();
+        if (resId == R.id.iv_title_left) {
+            getActivity().finish();
+        }
+    }
+
+    protected abstract String getTitle();
+
     @Override
     protected int getLayoutRes() {
         return R.layout.fragment_orm;
+    }
+
+    @Override
+    protected void bindView(View rootView) {
+        super.bindView(rootView);
+        ViewHelper.setOnClick(rootView, this, R.id.iv_title_left);
+    }
+
+    @Override
+    protected void init() {
+        mTitle = getTitle();
+        super.init();
     }
 
     @Override
